bff4aa1d7acc460effc226998ba223a4c58aca90
Kumar Kartikay <krkartikay@gmail.com>
2017-12-11 16:16:50 +0530

    solved first problem

---
+++ 1_aoc.py
@@ -0,0 +1,17 @@
+with open("1_input.txt",'r') as f:
+    digits = list(f.read())
+    digits.remove('\n')
+
+l = len(digits)
+digits.append(digits[0])
+digits = map(int,digits)
+
+repeat_digits = []
+
+for x in range(l-1):
+    curt_digit = digits[x]
+    next_digit = digits[x+1]
+    if curt_digit == next_digit:
+        repeat_digits.append(curt_digit)
+
+print sum(repeat_digits)
