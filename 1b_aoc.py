4884477eb39b90fd2fa4473c21ed0e96c7827411
Kumar Kartikay <krkartikay@gmail.com>
2017-12-11 16:38:21 +0530

    Done 1st problem part two

---
+++ 1b_aoc.py
@@ -0,0 +1,15 @@
+digits = list(raw_input())
+
+l = len(digits)
+digits.append(digits[0])
+digits = map(int,digits)
+
+repeat_digits = []
+
+for x in range(l):
+    curt_digit = digits[x]
+    next_digit = digits[(x+l/2)%l]
+    if curt_digit == next_digit:
+        repeat_digits.append(curt_digit)
+
+print sum(repeat_digits)
