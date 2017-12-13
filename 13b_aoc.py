2ee056a27dba28a394317bd25d77c4095b727933
Kumar Kartikay <krkartikay@gmail.com>
2017-12-13 14:17:45 +0530

    Finallllly solved 13B!!

--- 13b_aoc.py
+++ 13b_aoc.py
@@ -1,6 +1,6 @@
 import copy
+
 scanners = {}
-length = 0
 
 def main():
 	try:
@@ -11,68 +11,17 @@ def main():
 
 def process(inp):
 	l = map(int,inp.split(':'))
-	scanners[l[0]] = [0,l[1],1]
-
-def printscanners(j):
-	length = max(scanners.keys())+1
-	print str(j)+"th picosec"
-	for i in range(length):
-		if i in scanners:
-			#print scanners[i]
-			for x in range(scanners[i][1]):
-				if i==j and x==0:
-					print ("( )" if x!=scanners[i][0] else "(S)"),
-				else:
-					print ("[ ]" if x!=scanners[i][0] else "[S]"),
-		else:
-			print "...",
-		print ""
-	print "="*60
-
-def severity(delay):
-	length = max(scanners.keys())+1
-	sev = 0
-	for i in range(delay):
-		for j in range(length):
-			if j in scanners:
-				if not((scanners[j][0] == scanners[j][1]-1 and scanners[j][2]==1) or (scanners[j][0] == 0 and scanners[j][2]==-1)):
-					scanners[j][0] += scanners[j][2]
-				else:
-					scanners[j][2] *= (-1)
-					scanners[j][0] += scanners[j][2]
-	#print "After delay %d psec"%delay
-	#printscanners(i)
-	for i in range(length):
-		#printscanners(i)
-		if i in scanners:
-			if scanners[i][0] == 0:
-				return 100
-				sev += (i+1)*scanners[i][1]
-		for j in range(length):
-			if j in scanners:
-				if not((scanners[j][0] == scanners[j][1]-1 and scanners[j][2]==1) or (scanners[j][0] == 0 and scanners[j][2]==-1)):
-					scanners[j][0] += scanners[j][2]
-				else:
-					scanners[j][2] *= (-1)
-					scanners[j][0] += scanners[j][2]
-	return sev
+	scanners[l[0]] = l[1]
 
 def analyse_results():
-	global scanners
-	d = 0
-	sev = 1
-	while sev!=0:
-		sev = severity(d)
-		d+=2
-		reset_scanners()
-		if d%1000==0:
-			print d
-	print d-2
+	m = range(10000000)
+	def f(x):
+		for s in scanners:
+			if ((x+s)%(2*(scanners[s]-1)))==0:
+				return False
+		return True
+	m = filter(f,m)
+	print m
 
-def reset_scanners():
-	global scanners
-	for key in scanners:
-		scanners[key][0] = 0
-		scanners[key][2] = 1
 
 main()
