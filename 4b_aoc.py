c18af5f9c7667eaf64d043d50b931c0327a02f52
Kumar Kartikay <krkartikay@gmail.com>
2017-12-14 15:05:00 +0530

    New Logic

--- 4b_aoc.py
+++ 4b_aoc.py
@@ -1,33 +1,23 @@
-valid_passphrases = 0
+def main():
+    valid_passphrases = 0
+    try:
+        while True:
+            inp = raw_input()
+            if validate(inp):
+                valid_passphrases += 1
+    except EOFError:
+        print valid_passphrases
 
-def count(ch,word):
-    "Counts number of occourances of ch in word"
-    r = 0
-    for char in word:
-        if char==ch:
-            r += 1
-    return r
-
-def is_anagram(w1,w2):
-    if len(w1)!=len(w2):
-        return False
-    else:
-        for ch in w1:
-            if count(ch,w1) != count(ch,w2):
-                return False
-        return True
 
 def validate(passphrase):
     appeared_words = []
     for word in passphrase.split():
-        for checkword in appeared_words:
-            if is_anagram(word,checkword):
-                return False
+        l = list(word)
+        l.sort()
+        word = "".join(l)
+        if word in appeared_words:
+            return False
         appeared_words.append(word)
     return True
 
-try:
-    while True:
-        valid_passphrases += int(validate(raw_input()))
-except EOFError:
-    print valid_passphrases
+main()
