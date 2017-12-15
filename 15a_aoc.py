f76a517d68a9b43a626f09b585ecf52931ba13b7
Kumar Kartikay <krkartikay@gmail.com>
2017-12-15 10:23:15 +0530

    basic structure

---
+++ 15a_aoc.py
@@ -0,0 +1,16 @@
+import itertools as it
+
+data = {}
+
+def main():
+    try:
+        while True:
+            process(raw_input())
+    except EOFError:
+        analyse_results()
+
+def process(inp):
+    l = inp.split(' ')
+
+def analyse_results():
+    pass
