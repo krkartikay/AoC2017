01befe6c28e6a10993afeb9a5a825b9f1a20bade
Kumar Kartikay <krkartikay@gmail.com>
2017-12-15 10:51:13 +0530

    solved 15a!

--- 15a_aoc.py
+++ 15a_aoc.py
@@ -1,16 +1,35 @@
 import itertools as it
 
-data = {}
+def genA():
+    prevval = 65
+    factor = 16807
+    modulo = 2147483647
+    while True:
+        newval = (prevval*factor) % modulo
+        prevval = newval
+        yield newval
+
+def genB():
+    prevval = 8921
+    factor = 48271
+    modulo = 2147483647
+    while True:
+        newval = (prevval*factor) % modulo
+        prevval = newval
+        yield newval
 
 def main():
-    try:
-        while True:
-            process(raw_input())
-    except EOFError:
-        analyse_results()
+    gA,gB = genA(), genB()
+    total = 0
+    for x in range(40000000):
+        a,b = gA.next(), gB.next()
+        a,b = bin(a),bin(b)
+        a = a[:-17:-1]
+        b = b[:-17:-1]
+        if a==b:
+            total += 1
+        if x%10000==0:
+            print (x/400000),"%"
+    print total
 
-def process(inp):
-    l = inp.split(' ')
-
-def analyse_results():
-    pass
+main()
