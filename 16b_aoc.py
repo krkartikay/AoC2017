391d1882b3e8d2c363f2eff806b3c3f43fe29bd0
Kumar Kartikay <krkartikay@gmail.com>
2017-12-16 15:00:47 +0530

    SOLVED 16B sucksesfullay!!!

--- 16b_aoc.py
+++ 16b_aoc.py
@@ -1,40 +1,38 @@
 programs = [chr(ord('a')+x) for x in range(16)]
-partners = []
-tot_spin = 0
 
 def main():
     try:
         while True:
             i = raw_input()
-            for x in i.split(','):
-                process(x)
+            # for q in xrange(1000000000):
+            #     for x in i.split(','):
+            #         process(x)
+            #     if (q%10000000==0):
+            #         print q/1000000000,'%'
+            z = 0
+            while z<55: # magic
+                for x in i.split(','):
+                    process(x)
+                print "".join(programs)
+                z += 1
     except EOFError:
         analyse_results()
 
-def a_spin(x):
+def spin(x):
     global programs
     l = len(programs)
     programs = programs[-x:] + programs
     programs = programs[:l]
 
-def a_partner(a,b):
+def partner(a,b):
     i = programs.index(a)
     j = programs.index(b)
-    a_exchange(i,j)
+    exchange(i,j)
 
-def a_exchange(a,b):
+def exchange(a,b):
     k = programs[a]
     programs[a] = programs[b]
     programs[b] = k
-
-def spin(x):
-    a_spin(x)
-
-def partner(a,b):
-    pass
-
-def exchange(a,b):
-    a_exchange(a,b)
 
 def process(inp):
     if inp[0]=='s':
@@ -45,18 +43,9 @@ def process(inp):
     elif inp[0]=='p':
         a,b = inp[1],inp[3]
         partner(a,b)
+    #print "".join(programs)
 
 def analyse_results():
-    perm = [ord(x)-ord('a') for x in programs]
-    # now we have list of partners and default arrangement
-    # partners do not matter for even no of iterations
-    # for (a,b) in partners:
-    #     a_partner(a,b)
-    start = range(16)
-    for i in xrange(1000):
-        for j in xrange(1000000):
-            start = [start[x] for x in perm]
-        print i/10.,"%"
-    print "".join(chr(x+ord('a')) for x in start)
+    print "".join(programs)
 
 main()
