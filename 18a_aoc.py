3ba225aa9a59ba484ad2895ef185f56764893592
Kumar Kartikay <krkartikay@gmail.com>
2017-12-18 10:41:56 +0530

    solved!

--- 18a_aoc.py
+++ 18a_aoc.py
@@ -1,16 +1,59 @@
-g = {}
+import sys
+
+regs = {}
+lastPlayed = 0
+program = []
 
 def main():
+    for x in range(26):
+        regs[chr(ord('a')+x)] = 0
     try:
         while True:
-            process(raw_input())
+            getinputs(raw_input())
     except EOFError:
         solve()
 
+def getinputs(arg):
+    program.append(arg)
+
 def process(arg):
-    pass
+    global lastPlayed
+    arg = arg.split()
+    print arg
+    if arg[0]=='snd':
+        lastPlayed = regs[arg[1]]
+    elif arg[0]=='set':
+        if arg[2] in regs:
+            regs[arg[1]] = regs[arg[2]]
+        else:
+            regs[arg[1]] = int(arg[2])
+    elif arg[0]=='add':
+        if arg[2] in regs:
+            regs[arg[1]] += regs[arg[2]]
+        else:
+            regs[arg[1]] += int(arg[2])
+    elif arg[0]=='mul':
+        if arg[2] in regs:
+            regs[arg[1]] *= regs[arg[2]]
+        else:
+            regs[arg[1]] *= int(arg[2])
+    elif arg[0]=='mod':
+        if arg[2] in regs:
+            regs[arg[1]] %= regs[arg[2]]
+        else:
+            regs[arg[1]] %= int(arg[2])
+    elif arg[0]=='rcv':
+        if lastPlayed != 0:
+            print lastPlayed
+            sys.exit(0)
+    elif arg[0]=='jgz':
+        if regs[arg[1]]>0:
+            return int(arg[2])
+    return 1
 
 def solve():
-    pass
-
+    i = 0
+    while i<len(program):
+        line = program[i]
+        i += process(line)
 main()
