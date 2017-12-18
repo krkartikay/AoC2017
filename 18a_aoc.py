9faa62c4b96a003d118561da07cfbc6376e645ab
Kumar Kartikay <krkartikay@gmail.com>
2017-12-18 10:08:50 +0530

    template

---
+++ 18a_aoc.py
@@ -0,0 +1,16 @@
+g = {}
+
+def main():
+    try:
+        while True:
+            process(raw_input())
+    except EOFError:
+        solve()
+
+def process(arg):
+    pass
+
+def solve():
+    pass
+
+main()
