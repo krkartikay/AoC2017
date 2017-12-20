1bc803042386bf9697f3f45de4a99bb7db59a5f4
Kumar <krkartikay@gmail.com>
2017-12-20 10:46:46 +0530

    solved 20A

--- 20a_aoc.py
+++ 20a_aoc.py
@@ -1,4 +1,8 @@
+import re
+pat = re.compile(r'^p=<(.*)>, v=<(.*)>, a=<(.*)>')
+
 l = []
+d = []
 
 def main():
     try:
@@ -8,7 +12,24 @@ def main():
         solve()
 
 def process(arg):
-    pass
+    m = pat.match(arg)
+    p,v,a = [tuple(map(int, m.group(x).split(',')))
+            for x in range(1,4)]
+    l.append((p,v,a))
+
+def dist(pt):
+    x,y,z = pt
+    x = abs(x)
+    y = abs(y)
+    z = abs(z)
+    return x+y+z
 
 def solve():
-    pass
+    for particle in l:
+        p,v,a = particle
+        # after 1000 ticks
+        p = tuple(p[i] + v[i]*1000 + (a[i]*1000**2)/2 for i in range(3))
+        d.append(dist(p))
+    print d.index(min(d))
+
+main()
