8b2d8bf751c27288174427593593deb400897a01
Kumar Kartikay <krkartikay@gmail.com>
2017-12-23 12:45:42 +0530

    FINALLY!!! 18B

--- 18b_aoc.py
+++ 18b_aoc.py
@@ -18,16 +18,26 @@ class ProcessProgram():
         self.pid = pid
         self.regs = {}
         self.recieveQueue = []
-        for x in range(26):
-            self.regs[chr(ord('a')+x)] = 0
+        # for x in range(26):
+        #     self.regs[chr(ord('a')+x)] = 0
         self.regs['p'] = pid
         self.rip = 0
         self.other = None
+
+    def get(self,arg):
+        if 'a'<=arg<='z':
+            if arg in self.regs:
+                return self.regs[arg]
+            else:
+                self.regs[arg] = 0
+                return 0
+        else:
+            return int(arg)
+
     def process(self):
         global timesSent
         arg = program[self.rip]
         arg = arg.split()
-        print arg, '\t\t', self.pid, self.rip
         if arg[0]=='snd':
             if self.pid == 1:
                 timesSent += 1
@@ -38,52 +48,51 @@ class ProcessProgram():
             self.rip += 1
             return 1
         elif arg[0]=='set':
-            if arg[2] in self.regs:
-                self.regs[arg[1]] = self.regs[arg[2]]
-            else:
-                self.regs[arg[1]] = int(arg[2])
+                self.regs[arg[1]] =  self.get(arg[2])
         elif arg[0]=='add':
-            if arg[2] in self.regs:
-                self.regs[arg[1]] += self.regs[arg[2]]
-            else:
-                self.regs[arg[1]] += int(arg[2])
+                self.regs[arg[1]] += self.get(arg[2])
         elif arg[0]=='mul':
-            if arg[2] in self.regs:
-                self.regs[arg[1]] *= self.regs[arg[2]]
-            else:
-                self.regs[arg[1]] *= int(arg[2])
+                self.regs[arg[1]] *= self.get(arg[2])
         elif arg[0]=='mod':
-            if arg[2] in self.regs:
-                self.regs[arg[1]] %= self.regs[arg[2]]
-            else:
-                self.regs[arg[1]] %= int(arg[2])
+                self.regs[arg[1]] %= self.get(arg[2])
         elif arg[0]=='rcv':
             if len(self.recieveQueue)>0:
                 self.regs[arg[1]] = self.recieveQueue.pop()
             else:
-                self.rip += 1
                 return -1
         elif arg[0]=='jgz':
-            if arg[1] in self.regs:
-                z = self.regs[arg[1]]
-            else:
-                z = int(arg[1])
+            z = self.get(arg[1])
             if z>0:
-                if arg[2] in self.regs:
-                    self.rip += self.regs[arg[2]]
-                else:
-                    self.rip += int(arg[2])
+                self.rip += self.get(arg[2])
+                self.printState()
+                return 0
+        else:
+            raise Exception("Instruction not found")
+
+        self.printState()
+
         self.rip += 1
         return 0
+
+    def printState(self):
+        if self.pid==0:
+            print "%2d: <%10s>"%(self.rip,program[self.rip])
+            print "\t\t\t\t\t",self.regs,self.recieveQueue
+        else:
+            print "\t\t%2d: <%10s>"%(self.rip,program[self.rip])
+            print "\t\t\t\t\t",self.regs,self.recieveQueue
+
+import time
 
 def solve():
     p0 = ProcessProgram(0)
     p1 = ProcessProgram(1)
     p0.other = p1
     p1.other = p0
-    readyQ = [p0,p1]
+    readyQ = [p1,p0]
     waitingQ = []
     while len(readyQ)>0:
+        #time.sleep(0.01)
         p = readyQ.pop()
         r = p.process()
         if r==-1:
@@ -95,7 +104,8 @@ def solve():
             except IndexError:
                 pass
         readyQ.insert(0,p)
-
+        assert len(readyQ)+len(waitingQ)==2
+    print timesSent
 
 def end():
     print timesSent
