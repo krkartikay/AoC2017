a5b52895bb50bafc0d2c9430bd570e81e50be516
Kumar Kartikay <krkartikay@gmail.com>
2017-12-24 20:04:21 +0530

    Solved 24a... in 1st attempt :)

--- 24a_aoc.py
+++ 24a_aoc.py
@@ -1,4 +1,4 @@
-l = {}
+ports = []
 
 def main():
     try:
@@ -8,9 +8,32 @@ def main():
         solve()
 
 def process(arg):
-    pass
+    ports.append(tuple(int(x) for x in arg.split("/")))
 
-def solve(arg):
-    pass
+def solve():
+    print max(strength(bridge) for bridge in possible_bridges((0,),ports))
+
+def strength(bridge):
+    return 2*sum(bridge)-bridge[0]-bridge[-1]
+
+def possible_bridges(beginning,pieces):
+    bridges = [beginning]
+    #print beginning, pieces
+    if len(pieces)==0:
+        return bridges
+    for (x,y) in pieces:
+        if beginning[-1] == x:
+            new = beginning + (y,)
+            z = pieces[:]
+            z.remove((x,y))
+            bridges += possible_bridges(new, z)
+        elif beginning[-1] == y:
+            new = beginning + (x,)
+            z = pieces[:]
+            z.remove((x,y))
+            bridges += possible_bridges(new, z)
+        else:
+            pass
+    return bridges
 
 main()
